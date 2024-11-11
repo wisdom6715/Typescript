547b058506060720fae41899f2b72757121faf2c
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-11-11 17:35:17 +0100

    done with enums

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,15 @@ let age = 21;
 if (age > 20) {
     age += 10;
 }
-console.log(age);
+let course = 'typescript';
+let sales = 123243243;
+function wisdom(document) {
+    console.log(document);
+}
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+number.forEach(n => n.toExponential);
+let user = [1, 'mosh'];
+user.push(1);
+let mySize = 1;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ if (age > 20){
     age += 10;
 }
     
-console.log(age);
+// console.log(age);
 
 //// types inn typescript
 let course = 'typescript';
@@ -12,3 +12,13 @@ function wisdom(document: any){
     console.log(document);
     
 }
+
+let number: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 10]
+number.forEach(n => n.toExponential)
+
+let user: [number, string] = [1, 'mosh'];
+user.push(1);
+// Moving to enus
+const enum Size {Small=1, Medium, Large}
+let mySize: Size = Size.Small
+console.log(mySize);
