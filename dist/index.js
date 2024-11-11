d58ec6aa44614ad2a13c391f487342d6c43e867b
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-11-11 17:08:18 +0100

    starting with typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 21;
+if (age > 20) {
+    age += 10;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let age: number = 21
+if (age > 20){
+    age += 10;
+}
+    
+console.log(age);
+
+//// types inn typescript
+let course = 'typescript';
+let sales = 123_243_243
+function wisdom(document: any){
+    console.log(document);
+    
+}
